f869cd65b55d72cd4c2894c61adea92e2bb3207d
Rex Bergeron <rexbergeron@comcast.net>
2020-01-31 14:30:15 -0800

    almost there

--- map/has-completed-all-quests.js
+++ map/has-completed-all-quests.js
@@ -2,7 +2,7 @@ function hasCompletedAllQuests(quests, user) {
     for (let i = 0; i < quests.length; i++) {
         const quest = quests[i];
 
-        if(!user.completed[quest.id]) {
+        if (!user.completed[quest.id]) {
             return false;
         }
     }
